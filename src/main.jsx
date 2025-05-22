9742c5f7f9be74469d8b0ef5fe72d376c6bcc0be
Kenneth Tay <kenneth21tay@gmail.com>
2025-05-22 16:15:11 +0800

    Updated disclaimer

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <h1>This is a simple CV/Resume builder</h1>
-    <p>Disclaimer: Use a laptop or desktop for better performance.</p>
+    <p>Disclaimer: This is just a project for me to learn REACT(components and states). 
+      Do not use it as a real template for your resume.</p>
     <App />
   </StrictMode>,
 )
