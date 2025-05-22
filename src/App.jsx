84ac875775557080b27eaf9a805bfb66cccc20c9
Kenneth Tay <kenneth21tay@gmail.com>
2025-05-22 15:48:26 +0800

    Change implementation of GeneralInformation, so that in I can implement react pdf in the future.

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,22 @@ import Skills from './Components/Skills'
 import './App.css'
 
 function App() {
+    const [cvData, setCvData] = useState({
+        generalInfo: {
+        fullName: '',
+        email: '',
+        phone: '',
+        location: '',
+        link: ''},
+        educations: [{id:1, 
+        }],
+        experiences: [1],
+    })
+
     return (
         <>
-            <GeneralInformation />
+            <GeneralInformation data={cvData.generalInfo}
+            onChange={(newInfo) => setCvData((prev) => ({ ...prev, generalInfo: newInfo }))}/>
             <Educations />
             <Experiences />
             <Projects />
--- src/Components/GeneralInformation.jsx
+++ src/Components/GeneralInformation.jsx
@@ -1,14 +1,19 @@
-function GeneralInformation() {
+function GeneralInformation({ data, onChange }) {
     return (<>
-        <b><input type="text" placeholder="Name" style={{fontSize:"2em",textAlign:"center"}} className="nameInput"/></b>
+        <b><input type="text" placeholder="Name" style={{fontSize:"2em",textAlign:"center"}} className="nameInput"
+        onChange={(e) => onChange({ ...data, fullName: e.target.value })}/></b>
         <div className="generalInfoContainer">
-            <input type="text" placeholder="Phone Number" className="phoneInput"/>
+            <input type="text" placeholder="Phone Number" className="phoneInput"
+            onChange={(e) => onChange({ ...data, phone: e.target.value })}/>
             <span>|</span>
-            <input type="email" placeholder="Email" className="emailInput"/>
+            <input type="email" placeholder="Email" className="emailInput"
+            onChange={(e) => onChange({ ...data, email: e.target.value })}/>
             <span>|</span>
-            <input type="text" placeholder="Location" className="locationInput"/>
+            <input type="text" placeholder="Location" className="locationInput"
+            onChange={(e) => onChange({ ...data, lcoation: e.target.value })}/>
             <span>|</span>
-            <input type="link" placeholder="LinkedIn / Github" className="linkInput"/>
+            <input type="link" placeholder="LinkedIn / Github" className="linkInput"
+            onChange={(e) => onChange({ ...data, link: e.target.value })}/>
         </div>
     </>
     )
